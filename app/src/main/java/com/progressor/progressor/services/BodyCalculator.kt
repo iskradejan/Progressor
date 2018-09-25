93d9adac81fa7a945c6c8a1825234681cb3681ac
Dejan Iskra <>
2018-09-24 22:04:22 -0400

    body formulas reformatted

--- app/src/main/java/com/progressor/progressor/services/BodyCalculator.kt
+++ app/src/main/java/com/progressor/progressor/services/BodyCalculator.kt
@@ -5,26 +5,17 @@ import java.text.DecimalFormat
 class BodyCalculator {
     private val format = DecimalFormat("#.##")
 
-    fun calculateMaleFat(weight: Double, waist: Double) : Double {
-        val firstStep = ( weight * 1.082 ) + 94.42
-        val secondStep = firstStep - ( waist * 4.15 )
-        val thirdStep = ( ( weight - secondStep ) * 100 ) / weight
+    fun calculateMaleFat(weight: Double, waist: Double): Double {
 
-        return format.format(thirdStep).toDouble()
+        val fat = ((weight - (((weight * 1.082) + 94.42) - (waist * 4.15))) * 100) / weight
+
+        return format.format(fat).toDouble()
     }
 
-    fun calculateFemaleFat(weight: Double, wrist: Double, waist: Double, hip: Double, forearm: Double) : Double {
-        val firstStep = ( weight * 0.732 ) + 8.987
-        val secondStep = wrist / 3.14
-        val thirdStep = waist * 0.157
-        val fourthStep = hip * 0.249
-        val fifthStep = forearm * 0.434
-        val sixthStep = firstStep + secondStep
-        val seventhStep = sixthStep - thirdStep
-        val eightStep = seventhStep - fourthStep
-        val ninthStep = fifthStep + eightStep
-        val tenthStep = ( ( weight - ninthStep) * 100) / weight
+    fun calculateFemaleFat(weight: Double, wrist: Double, waist: Double, hip: Double, forearm: Double): Double {
 
-        return format.format(tenthStep).toDouble()
+        val fat = ((weight - ((forearm * 0.434) + (((((weight * 0.732) + 8.987) + (wrist / 3.14)) - (waist * 0.157)) - (hip * 0.249)))) * 100) / weight
+
+        return format.format(fat).toDouble()
     }
 }
