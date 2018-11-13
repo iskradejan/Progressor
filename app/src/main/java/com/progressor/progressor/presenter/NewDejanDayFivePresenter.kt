95765125004a2936e8fdc5d6aef9b2358145772b
Dejan Iskra <>
2018-11-12 21:28:07 -0500

    day four, five done

--- app/src/main/java/com/progressor/progressor/presenter/NewDejanDayFivePresenter.kt
+++ app/src/main/java/com/progressor/progressor/presenter/NewDejanDayFivePresenter.kt
@@ -1,6 +1,8 @@
 package com.progressor.progressor.presenter
 
 import android.content.Context
+import com.progressor.progressor.model.constant.FirebaseConstant
+import com.progressor.progressor.model.dataobjects.workout.dejan.DayFive
 import com.progressor.progressor.service.AuthenticationManager
 import com.progressor.progressor.service.FragmentNavigator
 import com.progressor.progressor.service.UserManager
@@ -16,11 +18,52 @@ class NewDejanDayFivePresenter @Inject constructor(
     private var context: Context? = null
 
     interface View {
-
+        fun isFormValid(): Boolean
+        fun getSquats(): List<String>
+        fun getRomanianDeadlifts(): List<String>
+        fun getPistolSquats(): List<String>
+        fun getLunges(): List<String>
+        fun getCableFrontRaise(): List<String>
+        fun getPushPress(): List<String>
+        fun getDumbbellLateralRaise(): List<String>
+        fun getHangingLegRaise(): List<String>
+        fun getBicycle(): List<String>
+        fun getRussianTwists(): List<String>
     }
 
     fun setPresenter(newDejanDayFiveFragment: NewDejanDayFiveFragment) {
         view = newDejanDayFiveFragment
         context = newDejanDayFiveFragment.context
+    }
+
+    fun save() {
+        if (view.isFormValid()) {
+            val dayFive = DayFive(
+                    squats = view.getSquats(),
+                    russianTwists = view.getRussianTwists(),
+                    bicycles = view.getBicycle(),
+                    hangingLegRaise = view.getHangingLegRaise(),
+                    dumbbellLateralRaise = view.getDumbbellLateralRaise(),
+                    pushPress = view.getPushPress(),
+                    cableFrontRaise = view.getCableFrontRaise(),
+                    lunges = view.getLunges(),
+                    pistolSquats = view.getPistolSquats(),
+                    romanianDeadlift = view.getRomanianDeadlifts()
+            )
+
+            userManager.user?.workout?.dejan?.dayFiveList?.let {
+                it.add(dayFive)
+            } ?: run {
+                val dayFiveList: MutableList<DayFive> = ArrayList()
+                dayFiveList.add(dayFive)
+                userManager.user?.workout?.dejan?.dayFiveList = dayFiveList
+            }
+
+            authenticationManager.firebaseUser?.uid?.let { uid ->
+                userManager.user?.let { user ->
+                    userManager.addUser(uid, user, FirebaseConstant.TYPE_DEJAN_DAY_FIVE)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/progressor/progressor/view/NewDejanDayFiveFragment.kt
+++ app/src/main/java/com/progressor/progressor/view/NewDejanDayFiveFragment.kt
@@ -1,7 +1,9 @@
 package com.progressor.progressor.view
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import com.progressor.progressor.R
 import com.progressor.progressor.di.components.MainComponentInterface
 import com.progressor.progressor.presenter.NewDejanDayFivePresenter
@@ -11,7 +13,7 @@ import javax.inject.Inject
 class NewDejanDayFiveFragment : BaseFragment(), NewDejanDayFivePresenter.View {
     @Inject
     lateinit var presenter: NewDejanDayFivePresenter
-
+    lateinit var defaultLabelColor: ColorStateList
     lateinit var currentView: View
 
     private fun initialize() {
@@ -27,6 +29,106 @@ class NewDejanDayFiveFragment : BaseFragment(), NewDejanDayFivePresenter.View {
         newDejanWorkoutDayFour.setOnClickListener {
             fragmentNavigator.to(NewDejanDayFourFragment())
         }
+        newDejanDayFiveSaveButton.setOnClickListener {
+            presenter.save()
+        }
+    }
+
+    private fun getSets(): ArrayList<EditText> {
+        return arrayListOf<EditText>(newDejanDayFiveSquatsSets, newDejanDayFiveRomanianDeadliftSets, newDejanDayFivePistolSquatsSets, newDejanDayFiveLungesSets, newDejanDayFiveCableFrontRaiseSets, newDejanDayFivePushPressSets, newDejanDayFiveDumbbellLateralRaiseSets, newDejanDayFiveHangingLegRaiseSets, newDejanDayFiveBicyclesSets, newDejanDayFiveRussianTwistsSets)
+    }
+
+    private fun getReps(): ArrayList<EditText> {
+        return arrayListOf<EditText>(newDejanDayFiveSquatsReps, newDejanDayFiveRomanianDeadliftReps, newDejanDayFivePistolSquatsReps, newDejanDayFiveLungesReps, newDejanDayFiveCableFrontRaiseReps, newDejanDayFivePushPressReps, newDejanDayFiveDumbbellLateralRaiseReps, newDejanDayFiveHangingLegRaiseReps, newDejanDayFiveBicyclesReps, newDejanDayFiveRussianTwistsReps)
+    }
+
+    private fun getAmounts(): ArrayList<EditText> {
+        return arrayListOf<EditText>(newDejanDayFiveSquatsAmount, newDejanDayFiveRomanianDeadliftAmount, newDejanDayFivePistolSquatsAmount, newDejanDayFiveLungesAmount, newDejanDayFiveCableFrontRaiseAmount, newDejanDayFivePushPressAmount, newDejanDayFiveDumbbellLateralRaiseAmount)
+    }
+
+    override fun getSquats(): List<String> {
+        return listOf(newDejanDayFiveSquatsSets.text.toString(), newDejanDayFiveSquatsReps.text.toString(), newDejanDayFiveSquatsAmount.text.toString())
+    }
+
+    override fun getRomanianDeadlifts(): List<String> {
+        return listOf(newDejanDayFiveRomanianDeadliftSets.text.toString(), newDejanDayFiveRomanianDeadliftReps.text.toString(), newDejanDayFiveRomanianDeadliftAmount.text.toString())
+    }
+
+    override fun getPistolSquats(): List<String> {
+        return listOf(newDejanDayFivePistolSquatsSets.text.toString(), newDejanDayFivePistolSquatsReps.text.toString(), newDejanDayFivePistolSquatsAmount.text.toString())
+    }
+
+    override fun getLunges(): List<String> {
+        return listOf(newDejanDayFiveLungesSets.text.toString(), newDejanDayFiveLungesReps.text.toString(), newDejanDayFiveLungesAmount.text.toString())
+    }
+
+    override fun getCableFrontRaise(): List<String> {
+        return listOf(newDejanDayFiveCableFrontRaiseSets.text.toString(), newDejanDayFiveCableFrontRaiseReps.text.toString(), newDejanDayFiveCableFrontRaiseAmount.text.toString())
+    }
+
+    override fun getPushPress(): List<String> {
+        return listOf(newDejanDayFivePushPressSets.text.toString(), newDejanDayFivePushPressReps.text.toString(), newDejanDayFivePushPressAmount.text.toString())
+    }
+
+    override fun getDumbbellLateralRaise(): List<String> {
+        return listOf(newDejanDayFiveDumbbellLateralRaiseSets.text.toString(), newDejanDayFiveDumbbellLateralRaiseReps.text.toString(), newDejanDayFiveDumbbellLateralRaiseAmount.text.toString())
+    }
+
+    override fun getHangingLegRaise(): List<String> {
+        return listOf(newDejanDayFiveHangingLegRaiseSets.text.toString(), newDejanDayFiveHangingLegRaiseReps.text.toString())
+    }
+
+    override fun getBicycle(): List<String> {
+        return listOf(newDejanDayFiveBicyclesSets.text.toString(), newDejanDayFiveBicyclesReps.text.toString())
+    }
+
+    override fun getRussianTwists(): List<String> {
+        return listOf(newDejanDayFiveRussianTwistsSets.text.toString(), newDejanDayFiveRussianTwistsReps.text.toString())
+    }
+
+    override fun isFormValid(): Boolean {
+        var invalid = 0
+
+        for(set in getSets()) {
+            context?.let { context ->
+                context.resources?.let { resources ->
+                    if(set.text.isEmpty() || set.text.length > 2) {
+                        set.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.baseMaroon, null))
+                        invalid++
+                    } else {
+                        set.backgroundTintList = defaultLabelColor
+                    }
+                }
+            }
+        }
+
+        for(rep in getReps()) {
+            context?.let { context ->
+                context.resources?.let { resources ->
+                    if(rep.text.isEmpty() || rep.text.length > 2) {
+                        rep.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.baseMaroon, null))
+                        invalid++
+                    } else {
+                        rep.backgroundTintList = defaultLabelColor
+                    }
+                }
+            }
+        }
+
+        for(amount in getAmounts()) {
+            context?.let { context ->
+                context.resources?.let { resources ->
+                    if(amount.text.isEmpty() || amount.text.length > 3) {
+                        amount.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.baseMaroon, null))
+                        invalid++
+                    } else {
+                        amount.backgroundTintList = defaultLabelColor
+                    }
+                }
+            }
+        }
+
+        return invalid == 0
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
